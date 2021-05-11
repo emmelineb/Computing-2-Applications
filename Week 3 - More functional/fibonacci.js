64375b83c6025186d6ae732a40033bece7d9fa6e
emmelineb <ehb20@ic.ac.uk>
2021-05-11 21:51:41 +0100

    changes

---
+++ Week 3 - More functional/fibonacci.js
@@ -0,0 +1,14 @@
+const curry = function (f) {
+    return function (first) {
+        return function (second) {
+            return f(first, second);
+        };
+    };
+};
+
+const add = function (x, y) {
+    return x + y;
+};
+
+const add_n = curry(add);
+console.log('hello thomas')
