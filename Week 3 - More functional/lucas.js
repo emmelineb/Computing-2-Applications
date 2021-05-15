fedecda6e0fb48afbfdba96b6b88197af83d8ba6
emmelineb <ehb20@ic.ac.uk>
2021-05-15 14:34:53 +0100

    works

---
+++ Week 3 - More functional/lucas.js
@@ -0,0 +1,11 @@
+function lucas(n) {
+    if (n === 0) {
+          return 2;
+    } else if (n === 1) {
+        return 1;
+    } else {
+        return (lucas(n - 1) + lucas(n - 2));
+    }
+  }
+
+debugger;
