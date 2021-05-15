86e6dcaaad2c911f7a3f87360b8a8ca9c174abce
emmelineb <ehb20@ic.ac.uk>
2021-05-15 14:50:14 +0100

    started

---
+++ Week 3 - More functional/connect_four.js
@@ -0,0 +1,11 @@
+const board = [ 
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+];
+
+new_board = () => board;
+
+red_move = (number) => (board) => board;
+yellow_move = (number) => (board) => board;
